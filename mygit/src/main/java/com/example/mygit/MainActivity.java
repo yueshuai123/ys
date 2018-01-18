37390cdd1299b758df190f58a1cba58d7fe5d96a
鬼鬼 <2861668923@qq.com>
2018-01-18 16:26:58 +0800

    我要提交

--- mygit/src/main/java/com/example/mygit/MainActivity.java
+++ mygit/src/main/java/com/example/mygit/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String mStr="aaaa";
+        //冲突下
+        String mStr1="冲突";
     }
 }
